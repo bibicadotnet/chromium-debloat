644e16ee55d4c230628f1d4842cf0a04f158970d
bibica <119997727+bibicadotnet@users.noreply.github.com>
2025-07-11 12:10:30 +0700

    Update chromium.bat

--- chromium.bat
+++ chromium.bat
@@ -1,3 +1,11 @@
 @echo off
+REM Check for admin privileges
+NET SESSION >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO Requesting administrative privileges...
+    powershell -Command "Start-Process -FilePath '%~dpnx0' -Verb RunAs"
+    EXIT /B
+)
+
 powershell -Command "Invoke-WebRequest 'https://go.bibica.net/chromium' -OutFile 'chromium.ps1'; powershell -ExecutionPolicy Bypass -File 'chromium.ps1'"
 pause
